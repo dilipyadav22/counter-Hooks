b09f7280ba9fbdea66eaacc1b4a373daceb28678
Dilip Kumar Yadav <yadavdip32@gmail.com>
2024-10-15 15:10:05 +0530

    conditions in counter

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { useState } from "react";
 
 const App =()=>{
   const [counter , setcounter] = useState(0);
+ const handel1=()=>{
+  if(counter<20)
+  setcounter(counter+1);
+ }
 
- const handel2=()=> setcounter(counter-1);
-
+ function handel2(){
+  if(counter>-10)
+   setcounter(counter-1)
+ }
   return(
     <div>
       <h1>{counter}</h1>
-    <button onClick={()=> setcounter(counter+1)} >+</button>
+    <button onClick={handel1} >+</button>
 
     <button onClick={handel2} >-</button>
     </div>
